ca2b56864a6acc3bd9c4596d39bdd11794cde385
Sumaiya Aktar <sumaiyaaktar3029@gmail.com>
2022-08-30 19:05:12 +0600

    add array with out sort function

---
+++ arraywithoutfuntion.php
@@ -0,0 +1,15 @@
+<?php 
+$array = array(1, 6, 23, 10, 3, 2, 15,7);
+
+for ($i=0; $i < count($array); $i++) { 
+        for ($j=$i+1; $j < count($array); $j++) { 
+            if($array[$i] > $array[$j]) {
+                $temp = $array[$i];
+                $array[$i] = $array[$j];
+                $array[$j] = $temp;
+            }
+        }
+    }
+    echo '<pre>';
+    print_r($array);
+?>
